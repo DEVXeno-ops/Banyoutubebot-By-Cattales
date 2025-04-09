ab90b39047d481f7749afadc558344c99975a102
DEVXeno-ops <steastea77@gmail.com>
2025-04-09 14:02:14 +0700

    Add files via upload

--- banbotspam/Form1.Designer.cs
+++ banbotspam/Form1.Designer.cs
@@ -33,139 +33,154 @@
             textBox4 = new TextBox();
             label5 = new Label();
             SuspendLayout();
+
             // 
-            // button1
+            // button1 (Ban Button)
             // 
-            button1.Font = new Font("Leelawadee UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(44, 168);
+            button1.Font = new Font("Leelawadee UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(30, 160);
             button1.Name = "button1";
-            button1.Size = new Size(215, 102);
+            button1.Size = new Size(200, 80);
             button1.TabIndex = 0;
             button1.Text = "แบน!!";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+
             // 
-            // button2
+            // button2 (Fetch Button)
             // 
-            button2.Font = new Font("Leelawadee UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(44, 36);
+            button2.Font = new Font("Leelawadee UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(30, 50);
             button2.Name = "button2";
-            button2.Size = new Size(215, 102);
+            button2.Size = new Size(200, 80);
             button2.TabIndex = 1;
             button2.Text = "ดูด";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
+
             // 
-            // richTextBox1
+            // richTextBox1 (Logs/Output)
             // 
-            richTextBox1.Location = new Point(310, 12);
+            richTextBox1.Font = new Font("Consolas", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(260, 12);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(1046, 621);
+            richTextBox1.Size = new Size(1100, 600);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
+
             // 
             // buttonSelectSecret
             // 
-            buttonSelectSecret.Font = new Font("Leelawadee UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            buttonSelectSecret.Location = new Point(44, 308);
+            buttonSelectSecret.Font = new Font("Leelawadee UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonSelectSecret.Location = new Point(30, 260);
             buttonSelectSecret.Name = "buttonSelectSecret";
-            buttonSelectSecret.Size = new Size(215, 102);
+            buttonSelectSecret.Size = new Size(200, 60);
             buttonSelectSecret.TabIndex = 4;
             buttonSelectSecret.Text = "เลือก secret";
             buttonSelectSecret.UseVisualStyleBackColor = true;
             buttonSelectSecret.Click += button4_Click;
+
             // 
-            // textBox1
+            // textBox1 (Channel Url)
             // 
-            textBox1.Location = new Point(44, 437);
+            textBox1.Location = new Point(30, 360);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(215, 23);
+            textBox1.Size = new Size(200, 23);
             textBox1.TabIndex = 5;
+
             // 
-            // label1
+            // label1 (Channel Url Label)
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(44, 419);
+            label1.Location = new Point(30, 340);
             label1.Name = "label1";
             label1.Size = new Size(69, 15);
             label1.TabIndex = 8;
             label1.Text = "Channel Url";
+
             // 
-            // textBox2
+            // textBox2 (Video Url)
             // 
-            textBox2.Location = new Point(35, 600);
+            textBox2.Location = new Point(30, 500);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(215, 23);
+            textBox2.Size = new Size(200, 23);
             textBox2.TabIndex = 9;
+
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(35, 582);
+            label2.Location = new Point(30, 480);
             label2.Name = "label2";
             label2.Size = new Size(189, 15);
             label2.TabIndex = 10;
             label2.Text = "YouTube Url (ถ้าต้องการเช็คตามคลิป)";
+
             // 
-            // checkBox2
+            // checkBox2 (Loop)
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(35, 550);
+            checkBox2.Location = new Point(30, 540);
             checkBox2.Name = "checkBox2";
             checkBox2.Size = new Size(50, 19);
             checkBox2.TabIndex = 11;
             checkBox2.Text = "loop";
             checkBox2.UseVisualStyleBackColor = true;
             checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+
             // 
-            // textBox3
+            // textBox3 (Loop Interval)
             // 
-            textBox3.Location = new Point(91, 548);
+            textBox3.Location = new Point(90, 538);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(32, 23);
+            textBox3.Size = new Size(40, 23);
             textBox3.TabIndex = 12;
             textBox3.TextChanged += textBox3_TextChanged;
+
             // 
-            // label3
+            // label3 (Minutes Label)
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Leelawadee UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(129, 539);
+            label3.Location = new Point(140, 541);
             label3.Name = "label3";
-            label3.Size = new Size(53, 32);
+            label3.Size = new Size(53, 15);
             label3.TabIndex = 13;
             label3.Text = "นาที";
+
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(44, 474);
+            label4.Location = new Point(30, 390);
             label4.Name = "label4";
             label4.Size = new Size(239, 15);
             label4.TabIndex = 15;
             label4.Text = "จำนวนคลิปที่ต้องการเช็ค (นับจากคลิปอัปโหลดล่าสุด)";
+
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(44, 492);
+            textBox4.Location = new Point(30, 408);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(29, 23);
+            textBox4.Size = new Size(40, 23);
             textBox4.TabIndex = 14;
+
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(86, 500);
+            label5.Location = new Point(80, 411);
             label5.Name = "label5";
             label5.Size = new Size(27, 15);
             label5.TabIndex = 16;
             label5.Text = "คลิป";
+
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1368, 635);
+            ClientSize = new Size(1380, 630);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(textBox4);
